5ab9c85fb476d4ea0280f1b2b4aaaf43729daf29
root <root@localhost.localdomain>
2014-02-04 18:59:40 +0100

    Added vorgotten config file.

--- src/mff/cuni/config/ConstantsMonteCarlo.java
+++ src/mff/cuni/config/ConstantsMonteCarlo.java
@@ -2,6 +2,7 @@ package mff.cuni.config;
 
 public class ConstantsMonteCarlo {
 	
+	public static double ACCEPTANCE_PROBABILITY = 0.95;
 	public static int DISKETTE_SIZE = 144;
 	
 	public static int repeat = 900000;
